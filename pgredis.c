6515353e75aaf7073888c11d11b079b76cbcef08
Daniel Gustafsson <daniel@hobbit.se>
2010-10-14 23:40:54 +0200

    Add support for selecting individual databases within a Redis instance and add support for setting values without requiring an expiration time.

--- pgredis.c
+++ pgredis.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2010 Daniel Gustafsson, Siavash Ghorbani, William Tisater
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /* 
  * pgredis
  * 
@@ -19,7 +41,7 @@ _PG_init(void) {
 	DefineCustomStringVariable(
 		"pgredis.redis_server",
 		"The Redis server to connect to",
-		"Redis server specified as host:port",
+		"Redis server specified as host[:port[:db]]",
 		&pgredis_server,
 		NULL,
 		PGC_USERSET,
@@ -52,13 +74,14 @@ guc_show_server_info(void) {
 	return (GucShowHook)pgredis->server;
 }
 
-
 /*
- * Helper functions
+ * add_redis_server - Adds a Redis server instance to connect to with an optional portnumber and
+ * Redis database number. The format for connect string is: <hostname>[:<port>[:<redis database>]]
  */
 static void
 add_redis_server(const char * host_port) {
 	char *tmp = (char *)host_port;
+	char *ptr;
 	int ret;
 
 	if (!pgredis) {
@@ -79,25 +102,43 @@ add_redis_server(const char * host_port) {
 	strncpy(pgredis->server, host_port, (tmp - host_port));
 	pgredis->server[(tmp - host_port)] = '\0';
 
-	if (*tmp == ':')
-		pgredis->port = atoi(++tmp);
-	else
+	if (*tmp == ':') {
+		pgredis->port = (int)strtod(++tmp, &ptr);
+		if (tmp == ptr) {
+			pgredis->port = PR_DEFAULT_PORT;
+			pgredis->database = PR_DEFAULT_DB;
+		}
+		else {
+			if (*ptr == ':') {
+				tmp = ++ptr;
+				pgredis->database = (int)strtod(tmp, &ptr);
+				if (tmp == ptr)
+					pgredis->database = PR_DEFAULT_DB;
+			}
+		}
+	}
+	else {
 		pgredis->port = PR_DEFAULT_PORT;
-
-	elog(LOG, "pgredis: added server: %s:%i", pgredis->server, pgredis->port);
+		pgredis->database = PR_DEFAULT_DB;
+	}
+	
+	elog(LOG, "pgredis: added server: %s:%i:%i", pgredis->server, pgredis->port, pgredis->database);
 
 	ret = redis_connect();
 	if (ret == PR_FAIL)
 		elog(ERROR, "pgredis: unable to connect to server");
+	else
+		elog(LOG, "pgredis: connected to server");
 }
 
 /*
- * Connect to the redis server. If the passed server struct is uninitialize it
+ * Connect to the redis server. If the passed server struct is uninitialized it
  * will be allocated prior to connection.
  * Returns PR_CONN if connection succeeded, else PR_FAILED 
  */
 static int
 redis_connect(void) {
+	int ret = 0;
 
 	if (!pgredis)
 		return PR_FAIL;
@@ -106,20 +147,20 @@ redis_connect(void) {
 		return PR_CONN;
 
 	pgredis->redis_server = credis_connect((pgredis->server ? pgredis->server : PR_DEFAULT_HOST), (pgredis->port > 0 ? pgredis->port : PR_DEFAULT_PORT), 2000);
-	return (pgredis->redis_server != NULL ? PR_CONN : PR_FAIL);
+	if (!pgredis->redis_server)
+		return PR_FAIL;
+
+	ret = credis_select(pgredis->redis_server, pgredis->database);			
+	return (ret == CREDIS_OK ? PR_CONN : PR_FAIL);
 }
 
 
 /*
  * DML Functions
  */
-Datum
-pr_set(PG_FUNCTION_ARGS) {
-	text * key_arg = PG_GETARG_TEXT_P(0);
-	text * value_arg = PG_GETARG_TEXT_P(1);
-	int expire = PG_GETARG_UINT32(2);
+static int
+pgredis_set(text *key_arg, text *value_arg, int expire_arg) {
 	int ret;
-	
 	char * key = NULL;
 	char * value = NULL;
 	
@@ -137,10 +178,27 @@ pr_set(PG_FUNCTION_ARGS) {
 	}
 
 	ret = credis_set(pgredis->redis_server, key, value);
-	if (ret == CREDIS_OK) {
-		ret = credis_expire(pgredis->redis_server, key, expire);
-	}
-  	PG_RETURN_BOOL(ret == CREDIS_OK);
+	if (ret == CREDIS_OK && expire_arg > 0)
+		ret = credis_expire(pgredis->redis_server, key, expire_arg);
+
+  	return ret;
+}
+
+Datum
+pr_set(PG_FUNCTION_ARGS) {
+	text * key_arg = PG_GETARG_TEXT_P(0);
+	text * value_arg = PG_GETARG_TEXT_P(1);
+
+	PG_RETURN_BOOL(pgredis_set(key_arg, value_arg, 0) == CREDIS_OK);
+}
+
+Datum
+pr_set_expire(PG_FUNCTION_ARGS) {
+	text * key_arg = PG_GETARG_TEXT_P(0);
+	text * value_arg = PG_GETARG_TEXT_P(1);
+	int expire_arg = PG_GETARG_UINT32(2);
+
+	PG_RETURN_BOOL(pgredis_set(key_arg, value_arg, expire_arg) == CREDIS_OK);
 }
 
 Datum
@@ -151,6 +209,28 @@ pr_set_server(PG_FUNCTION_ARGS) {
 	server = DatumGetCString(DirectFunctionCall1(textout, PointerGetDatum(server_arg)));
 	add_redis_server(server);
 	PG_RETURN_NULL();
+}
+
+Datum
+pr_select_db(PG_FUNCTION_ARGS) {
+	int database_arg = PG_GETARG_UINT32(0);
+	int ret = 0;
+
+	if (!pgredis || !pgredis->redis_server) {
+		elog(ERROR, "Redis server unspecified, unable to select database");
+		PG_RETURN_NULL();
+	}
+
+	if (database_arg < 0 || database_arg > 15)
+		PG_RETURN_FALSE;
+	
+	if (database_arg != pgredis->database) {
+		ret = credis_select(pgredis->redis_server, database_arg);
+		if (ret == CREDIS_OK)
+			pgredis->database = database_arg;
+	}
+	
+	PG_RETURN_BOOL(pgredis->database == database_arg);
 }
 
 Datum
@@ -183,3 +263,4 @@ pr_get(PG_FUNCTION_ARGS) {
 
 	PG_RETURN_NULL();
 }
+
--- pgredis.h
+++ pgredis.h
@@ -1,5 +1,26 @@
 #ifndef _PGREDIS_H_
 #define _PGREDIS_H_
+/*
+ * Copyright (c) 2010 Daniel Gustafsson, Siavash Ghorbani, William Tisater
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "postgres.h"
 
@@ -21,11 +42,19 @@
 #define PR_FAIL 0x02
 
 #define PR_DEFAULT_PORT 6789
+#define PR_DEFAULT_DB 0
 #define PR_DEFAULT_HOST "localhost"
 
+/*
+ * server - The hostname of the Redis server
+ * port - The portnumber of the Redis server
+ * database - The database within the Redis server to use
+ * redis_server - The database handle to communicate with Redis
+ */
 struct pr_server {
 	char *server;
 	int port;
+	int database;
 	REDIS redis_server;
 };
 
@@ -42,14 +71,21 @@ static GucShowHook guc_show_server_info(void);
 /* Helper function prototypes */
 static void add_redis_server(const char * host_port);
 static int redis_connect(void);
+static int pgredis_set(text *, text *, int);
 
 /* Function prototypes for pgredis stored procedures API */
 Datum pr_set(PG_FUNCTION_ARGS);
+Datum pr_set_expire(PG_FUNCTION_ARGS);
 Datum pr_get(PG_FUNCTION_ARGS);
+Datum pr_keys(PG_FUNCTION_ARGS);
+Datum pr_select_db(PG_FUNCTION_ARGS);
 Datum pr_set_server(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(pr_set);
+PG_FUNCTION_INFO_V1(pr_set_expire);
 PG_FUNCTION_INFO_V1(pr_get);
+PG_FUNCTION_INFO_V1(pr_keys);
+PG_FUNCTION_INFO_V1(pr_select_db);
 PG_FUNCTION_INFO_V1(pr_set_server);
 
 #endif /* _PGREDIS_H_ */
